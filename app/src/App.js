1fc3cfb4e08bf969c999bcf1fd9f3cc3f1500f80
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-09-28 03:26:45 +0500

    AOS

--- app/src/App.js
+++ app/src/App.js
@@ -12,9 +12,25 @@ import Blogs from './Components/Blogs/Blogs';
 import Blogs_Data from './Components/Blogs/Blogs_Data';
 import Travel_Reg_Form from './Components/Application_Form/Application_Form';
 import Travel_Reg_Form1 from './Components/Application_Form/Application_Form_1';
+import { useEffect} from 'react';
+import 'aos/dist/aos.css';  
+
+import AOS from 'aos';
 
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      easing: 'ease-in-out', 
+      once: true,
+      delay: 200, 
+    });
+  }, []);
+
+
+
   return (
  <div className="page-wrapper" style={{backgroundColor:'#232c27'}}>
 
